8a3a942ffa22d4270a1f87a187f52cbd86a1e96b
margaritasing <libiamfreitesm@gmail.com>
2022-11-29 11:44:21 -0300

    feature:agrego la libreria de gsap

--- src/bases/CounterEffect.tsx
+++ src/bases/CounterEffect.tsx
@@ -1,4 +1,5 @@
-import { useEffect, useState } from "react"
+import { useEffect, useState } from "react";
+import { gsap } from 'gsap';
 
 const MAXIMUN_COUNT = 10;
 
@@ -13,13 +14,17 @@ const handleClick = () => {
 
 useEffect(() => {
   if (counter < 10) return ; 
-  console.log('%cSe llego al valor máximo', 'color:red; background-color:black')
+  console.log('%cSe llego al valor máximo', 'color:red; background-color:black');
+    gsap.to('h2',{ y:-10, duration:0.2, ease: 'ease.out' }).then(()=> {
+    gsap.to('h2', {y:0, duration:1, ease:'bounce.out' }) //animaciones en react con esta libreria
+  })
 }, [counter])
 
 
   return (
     <div>
-       <h1>CounterEffect: { counter }</h1>
+       <h1>CounterEffect:</h1>
+       <h2>{ counter }</h2>
        <button onClick={handleClick}>+1</button>
     </div>
   )
