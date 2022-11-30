f493720f21767dedf87de2ce2d147f40254ae888
margaritasing <libiamfreitesm@gmail.com>
2022-11-30 20:04:25 -0300

    feature:agrego modificaciones a la parte de action

--- src/CounterReducer/actions/action.ts
+++ src/CounterReducer/actions/action.ts
@@ -1,3 +1,21 @@
 export type CounterAction = 
     | {type:'incremental', payload: { value:number}}
     | {type:'reset'}
+
+
+/* export const doReset = (): CounterAction => {
+    return {
+        type: 'reset'
+    }
+} */
+
+export const doReset = ():CounterAction => ({
+    type: 'reset'    
+})
+
+export const doIncremental = (value:number):CounterAction => ({
+    type: 'incremental',
+    payload: {
+        value:value
+    }
+})
